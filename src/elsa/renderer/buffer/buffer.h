aafe65e70a6cd1ecbf2449a621c430eb11949515
garra <garra_gwc@126.com>
2019-10-02 16:24:24 +0800

    abstract bufferlayout

--- src/elsa/renderer/buffer/buffer.h
+++ src/elsa/renderer/buffer/buffer.h
@@ -10,17 +10,68 @@
 
 
 #pragma once
+#include <vector>
+#include <string>
 #include "../rendererobject.h"
+#include "../../core.h"
 
 class Buffer : public RenderObject
 {
 public:
-    static Buffer* CreateVertex(unsigned int size, float* data);
-    static Buffer* CreateIndex(unsigned int size, unsigned int* data);
+    class Element
+    {
+    public:
+        enum class DataType { UnKnown = 0, Float, Float2, Float3, Float4, Mat3, Mat4, Int, Int2, Int3, Int4, UChar, UShort, UInt, Bool };
 
+    public:
+        Element(DataType type, bool normalized=false, const std::string& name="");
+        DataType Type() const { return m_type; }
+        bool Normalized() const { return m_normalized; }
+        unsigned int Offset() const { return m_offset; }
+        unsigned int Size() const ;
+        unsigned int Components() const;
+
+    private:
+        DataType m_type = DataType::UnKnown;
+        bool m_normalized = false;
+        std::string m_name;
+        unsigned int m_offset = 0;
+        friend class Buffer;
+    };
+
+    class Layout
+    {
+    public:
+        Layout() {}
+        Layout(const std::initializer_list<Element>& elements);
+        std::vector<Element>::const_iterator begin() const { return  m_elements.begin(); }
+        std::vector<Element>::const_iterator end() const { return m_elements.end(); }
+        unsigned int Stride() const { return m_stride; }
+        void Push(Element& element);
+
+    protected:
+        void _calculateOffsetAndStride(Element& e);
+
+    private:
+        std::vector<Element> m_elements;
+        unsigned int m_stride = 0;
+    };
+    
 public:
+    Buffer(unsigned int size);
+    virtual ~Buffer();
+
+    void SetLayout(const Layout& layout);
     unsigned int GetCount() const;
+    
+
+    static Buffer* CreateVertex(unsigned int size, float* data);
+    static Buffer* CreateIndex(unsigned int size, void* data);
 
 protected:
-    unsigned int m_count = 0;
+    virtual void _ApplyLayout() const;
+
+protected:
+    Layout m_layout;
+    unsigned int m_size = 0;
 };
--- src/elsa/renderer/buffer/buffer_opengl.cpp
+++ src/elsa/renderer/buffer/buffer_opengl.cpp
@@ -10,9 +10,10 @@
 
 
 #include "buffer_opengl.h"
-#include "glad/gl.h"
+#include "../../core.h"
 
-OpenGLBuffer::OpenGLBuffer()
+OpenGLBuffer::OpenGLBuffer(unsigned int size)
+    : Buffer(size)
 {
     glGenBuffers(1, &m_id);
 }
@@ -22,11 +23,44 @@ OpenGLBuffer::~OpenGLBuffer()
     glDeleteBuffers(1, &m_id);
 }
 
+GLenum OpenGLBuffer::_TypeFrom(Element::DataType dataType) const
+{
+    switch(dataType)
+    {
+        case Element::DataType::Float:   return GL_FLOAT;
+        case Element::DataType::Float2:  return GL_FLOAT;
+        case Element::DataType::Float3:  return GL_FLOAT;
+        case Element::DataType::Float4:  return GL_FLOAT;
+        case Element::DataType::Int:     return GL_INT;
+        case Element::DataType::Int2:    return GL_INT;
+        case Element::DataType::Int3:    return GL_INT;
+        case Element::DataType::Int4:    return GL_INT;
+        case Element::DataType::UChar:   return GL_UNSIGNED_BYTE;
+        case Element::DataType::UShort:  return GL_UNSIGNED_SHORT;
+        case Element::DataType::UInt:    return GL_UNSIGNED_INT;
+        case Element::DataType::Bool:    return GL_BOOL;
+        case Element::DataType::Mat3:    return GL_FLOAT; 
+        case Element::DataType::Mat4:    return GL_FLOAT; 
+        default: CORE_ASSERT(false, "UnKnown Buffer::DataType!"); return -1;
+    }
+}
+
 OpenGLVertexBuffer::OpenGLVertexBuffer(unsigned int size, float* data)
+    : OpenGLBuffer(size)
 {
     glBindBuffer(GL_ARRAY_BUFFER, m_id);
     glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
-    m_count = size/sizeof(float);
+}
+
+void OpenGLVertexBuffer::_ApplyLayout() const
+{
+    unsigned int location = 0;
+    for(const auto e : m_layout)
+    {
+        glEnableVertexAttribArray(location);
+        glVertexAttribPointer(location, e.Components(), _TypeFrom(e.Type()), e.Normalized()? GL_TRUE:GL_FALSE, m_layout.Stride(), (const void*)e.Offset());
+        location++;
+    }
 }
 
 void OpenGLVertexBuffer::Bind(unsigned int slot) const
@@ -39,11 +73,11 @@ void OpenGLVertexBuffer::Unbind() const
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
-OpenGLIndexBuffer::OpenGLIndexBuffer(unsigned int size, unsigned int* data)
+OpenGLIndexBuffer::OpenGLIndexBuffer(unsigned int size, void* data)
+    : OpenGLBuffer(size)
 {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_id);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
-    m_count = size/sizeof(unsigned int);
 }
 
 
@@ -57,3 +91,8 @@ void OpenGLIndexBuffer::Unbind() const
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
+GLenum OpenGLIndexBuffer::GetIndexType()
+{
+    const Element& e = *m_layout.begin();
+    return _TypeFrom(e.Type());
+}
